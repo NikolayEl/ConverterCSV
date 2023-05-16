bcfd978a9d5c8559f63861697ba974321ab26cc1
Nikolay Elshin <nikolay19830223@gmail.com>
2023-05-16 18:51:15 +0800

    button, menu, help and other

--- ConverterCSV/ConvertDefinition.h
+++ ConverterCSV/ConvertDefinition.h
@@ -4,7 +4,13 @@
 #define OnMenuClicked2     2
 #define OnMenuClicked3     3
 #define OnExitSoftware     4
-#define OnButtonClicked    5
+#define OnButton1Clicked   5
+#define OnButton2Clicked   6
+#define OnButton3Clicked   7
+#define OnClearField       8
+#define OnHelpSofrware     9
+
+HWND hEditControl;
 
 LRESULT CALLBACK SoftwareMainProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
--- ConverterCSV/ConverterCSV.cpp
+++ ConverterCSV/ConverterCSV.cpp
@@ -44,16 +44,28 @@ LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp
 		switch (wp)
 		{
 		case OnMenuClicked1:
-			MessageBoxA(hWnd, "File is open!", "MENU:", MB_OK);
-			break;
-		case OnButtonClicked:
-			MessageBoxA(hWnd, "Thanks!", "Button clicked", MB_OK);
+			SetWindowTextA(hEditControl, "File is open!");
 			break;
 		case OnMenuClicked2:
-			MessageBoxA(hWnd, "Menu 2 was clicked!", "Menu worked", MB_OK);
+			SetWindowTextA(hEditControl, "income calculated!");
 			break;
 		case OnMenuClicked3:
-			MessageBoxA(hWnd, "Menu 3 was clicked!", "Menu worked", MB_OK);
+			SetWindowTextA(hEditControl, "result saved");
+			break;
+		case OnButton1Clicked:
+			SetWindowTextA(hEditControl, "File is open!");
+			break;
+		case OnButton2Clicked:
+			SetWindowTextA(hEditControl, "income calculated!");
+			break;
+		case OnButton3Clicked:
+			SetWindowTextA(hEditControl, "result saved");
+			break;
+		case OnClearField:
+			SetWindowTextA(hEditControl, "");
+			break;
+		case OnHelpSofrware:
+			SetWindowTextA(hEditControl, "This program is written for personal use and is not intended for distribution. Elshin N.V.");
 			break;
 		case OnExitSoftware:
 			PostQuitMessage(0);
@@ -76,15 +88,18 @@ void MainWndAddMenus(HWND hwnd)
 {
 	HMENU RootMenu = CreateMenu();
 	HMENU SubMenu = CreateMenu();
+	HMENU HelpMenu = CreateMenu();
 
 	AppendMenu(SubMenu, MF_STRING, OnMenuClicked1, L"File open");
-	AppendMenu(SubMenu, MF_STRING, OnMenuClicked2, L"Menu 2");
-	AppendMenu(SubMenu, MF_STRING, OnMenuClicked3, L"Menu 3");
+	AppendMenu(SubMenu, MF_STRING, OnMenuClicked2, L"Calculate income");
+	AppendMenu(SubMenu, MF_STRING, OnMenuClicked3, L"Save result");
 	AppendMenu(SubMenu, MF_SEPARATOR, NULL, NULL);
 	AppendMenu(SubMenu, MF_STRING, OnExitSoftware, L"Exit");
+	AppendMenu(HelpMenu, MF_STRING, OnHelpSofrware, L"About");
 
 
-	AppendMenu(RootMenu, MF_POPUP, (UINT_PTR)SubMenu, L"Operation");
+	AppendMenu(RootMenu, MF_POPUP, (UINT_PTR)SubMenu, L"File");
+	AppendMenu(RootMenu, MF_POPUP, (UINT_PTR)HelpMenu, L"Help");
 
 	SetMenu(hwnd, RootMenu);
 }
@@ -92,7 +107,11 @@ void MainWndAddMenus(HWND hwnd)
 void MainWndAddWidgets(HWND hWnd)
 {
 	CreateWindowA("static", "Operation Status:", WS_VISIBLE | WS_CHILD, 5, 5, 990, 20,hWnd, NULL, NULL, NULL); //Название виджета
-	CreateWindowA("static", "...ready", WS_VISIBLE | WS_CHILD, 5, 5, 990, 20, hWnd, NULL, NULL, NULL); //
-	CreateWindowA("edit", "This is edit control", WS_VISIBLE | WS_CHILD, 5, 30, 990, 20, hWnd, NULL, NULL, NULL);
-	CreateWindowA("button", "Open file", WS_VISIBLE | WS_CHILD, 450, 350, 120, 30, hWnd, (HMENU)OnButtonClicked, NULL, NULL);
+	//CreateWindowA("static", "wating for the command", WS_VISIBLE | WS_CHILD, 5, 5, 990, 20, hWnd, NULL, NULL, NULL); //
+	hEditControl = CreateWindowA("edit", "wating for the command", WS_VISIBLE | WS_CHILD | ES_MULTILINE, 5, 5, 990, 330, hWnd, NULL, NULL, NULL);
+	CreateWindowA("button", "Open file", WS_VISIBLE | WS_CHILD, 50, 350, 120, 30, hWnd, (HMENU)OnButton1Clicked, NULL, NULL);
+	CreateWindowA("button", "Calculate income", WS_VISIBLE | WS_CHILD, 200, 350, 120, 30, hWnd, (HMENU)OnButton2Clicked, NULL, NULL);
+	CreateWindowA("button", "Save result", WS_VISIBLE | WS_CHILD, 350, 350, 120, 30, hWnd, (HMENU)OnButton3Clicked, NULL, NULL);
+	CreateWindowA("button", "Clear", WS_VISIBLE | WS_CHILD, 490, 350, 120, 30, hWnd, (HMENU)OnClearField, NULL, NULL);
+	CreateWindowA("button", "Exit", WS_VISIBLE | WS_CHILD, 800, 350, 120, 30, hWnd, (HMENU)OnExitSoftware, NULL, NULL);
 }
